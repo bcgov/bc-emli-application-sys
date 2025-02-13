4a7faca018d5a232aecafa03908871053be62a22
Navdeep Kaur <navdeep.kaur@dxcas.com>
2025-02-13 12:40:30 -0800

    fix: spec file updated

--- spec/models/permit_application_spec.rb
+++ spec/models/permit_application_spec.rb
@@ -13,10 +13,12 @@ RSpec.describe PermitApplication, type: :model do
 
     it do
       should define_enum_for(:status).with_values(
-               new_draft: 0,
-               newly_submitted: 1,
-               revisions_requested: 3,
-               resubmitted: 4
+           newly_submitted: 1,
+           revisions_requested: 3,
+           resubmitted: 4,
+           viewed: 5,
+           update_needed: 6,
+           accepted: 7
              )
     end
   end
