3737f59be68ee82e923f7672078237c7fcf676d2
Jerermy Watts <jeremy.watts@dxcas.com>
2025-03-03 17:05:54 -0800

    chore: update the configuration for ESLint and prettier

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,8 +1,17 @@
 export default {
-  plugins: ["prettier-plugin-organize-imports"],
   printWidth: 120,
-  requirePragma: false,
   tabWidth: 2,
-  trailingComma: "es5",
-  semi: false,
-}
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  jsxSingleQuote: false,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: 'always',
+  insertPragma: false,
+  proseWrap: 'preserve',
+  endOfLine: 'lf',
+  singleAttributePerLine: false,
+};
