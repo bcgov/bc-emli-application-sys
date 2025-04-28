3b436806460fe5a7dd76e96f955863b76bc02e6c
Darrel Siegle <darrel.siegle@dxcas.com>
2025-04-28 09:47:58 -0700

    refactor: updated participant login text

--- app/frontend/components/shared/bcservicecard/energy-coach/index.tsx
+++ app/frontend/components/shared/bcservicecard/energy-coach/index.tsx
@@ -1,20 +1,43 @@
-import { t } from "i18next"
-import React from "react"
-import { BCServiceInfoBlock } from "../info-block"
+import { t } from 'i18next';
+import React from 'react';
+import { Button, Flex, Heading, Link, ListItem, Text, UnorderedList } from '@chakra-ui/react';
+import { ArrowSquareOut, CaretRight, Phone } from '@phosphor-icons/react';
+import { RouterLinkButton } from '../navigation/router-link-button';
 
 export function EnergyCoachInfoBlock() {
+  // return (
+  //   <BCServiceInfoBlock
+  //     title={}
+  //     //description={t('auth.bcServiceCardInfo.energyCoach.description')}
+
+  //     ctaText={t('auth.bcServiceCardInfo.energyCoach.requestACall')}
+  //     ctaLink={t('auth.bcServiceCardInfo.energyCoach.ctaLink')}
+  //   />
+  // );
+
+  const bulletPoints = [
+    t('auth.bcServiceCardInfo.energyCoach.fillingRebate'),
+    t('auth.bcServiceCardInfo.energyCoach.rebateQuestions'),
+    t('auth.bcServiceCardInfo.energyCoach.virtualWalkThrough'),
+    //t("auth.bcServiceCardInfo.energyCoach.homeUpgrades"),
+  ];
+
   return (
-    <BCServiceInfoBlock
-      title={t("auth.bcServiceCardInfo.energyCoach.title")}
-      description={t("auth.bcServiceCardInfo.energyCoach.description")}
-      bulletPoints={[
-        t("auth.bcServiceCardInfo.energyCoach.fillingRebate"),
-        t("auth.bcServiceCardInfo.energyCoach.rebateQuestions"),
-        t("auth.bcServiceCardInfo.energyCoach.virtualWalkThrough"),
-        t("auth.bcServiceCardInfo.energyCoach.homeUpgrades"),
-      ]}
-      ctaText={t("auth.bcServiceCardInfo.energyCoach.requestACall")}
-      ctaLink={t("auth.bcServiceCardInfo.energyCoach.ctaLink")}
-    />
-  )
+    <Flex direction="column" gap={2} p={6} rounded="sm" borderWidth={1} borderColor="border.light" bg="greys.white">
+      <Heading as="h3" m={0} color="theme.blueAlt">
+        {t('auth.bcServiceCardInfo.energyCoach.title')}
+      </Heading>
+      {/* <Text fontSize="sm">{description}</Text> */}
+      <UnorderedList fontSize="sm" pl={2} mb={0}>
+        {bulletPoints.map((p) => (
+          <ListItem>{p}</ListItem>
+        ))}
+      </UnorderedList>
+      <Flex direction="row" align="center" gap={2}>
+        <Phone size={16} />
+        <Text flex={1}>{t('auth.bcServiceCardInfo.energyCoach.phoneNumber')}</Text>
+      </Flex>
+      <Text flex={1}>{t('auth.bcServiceCardInfo.energyCoach.phoneNumberHours')}</Text>
+    </Flex>
+  );
 }
