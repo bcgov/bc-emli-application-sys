f69d4bb324bdbf440b286675c82f7c6c7ac1d4d2
Navdeep Kaur <navdeep.kaur@dxcas.com>
2025-05-23 11:37:24 -0700

    refactor: modified user search functionality

--- app/frontend/components/shared/base/model-search-input.tsx
+++ app/frontend/components/shared/base/model-search-input.tsx
@@ -1,15 +1,16 @@
-import { InputGroupProps, InputProps } from "@chakra-ui/react"
-import { observer } from "mobx-react-lite"
-import React, { useCallback } from "react"
-import { ISearch } from "../../../lib/create-search-model"
-import { debounce } from "../../../utils/utility-functions"
-import { SearchInput } from "./search-input"
+import { InputGroupProps, InputProps } from '@chakra-ui/react';
+import { observer } from 'mobx-react-lite';
+import React, { useCallback } from 'react';
+import { ISearch } from '../../../lib/create-search-model';
+import { debounce } from '../../../utils/utility-functions';
+import { SearchInput } from './search-input';
 
 interface IProps<TSearchModel extends ISearch> {
-  searchModel: TSearchModel
-  inputGroupProps?: Partial<InputGroupProps>
-  inputProps?: Partial<InputProps>
-  debounceTimeInMilliseconds?: number
+  searchModel: TSearchModel;
+  inputGroupProps?: Partial<InputGroupProps>;
+  inputProps?: Partial<InputProps>;
+  debounceTimeInMilliseconds?: number;
+  setAllUsers?: (allUsers: boolean) => void;
 }
 
 export const ModelSearchInput = observer(function ModelSearchInput<TSearchModel extends ISearch>({
@@ -17,16 +18,18 @@ export const ModelSearchInput = observer(function ModelSearchInput<TSearchModel 
   inputGroupProps,
   inputProps,
   debounceTimeInMilliseconds = 500,
+  setAllUsers,
 }: IProps<TSearchModel>) {
-  const { setQuery, query, search } = searchModel
-  const debouncedSearch = useCallback(debounce(search, debounceTimeInMilliseconds), [search])
+  const { setQuery, query, search } = searchModel;
+  const debouncedSearch = useCallback(debounce(search, debounceTimeInMilliseconds), [search]);
 
   const onSearch = (query: string) => {
-    setQuery(query)
-    debouncedSearch()
-  }
+    setQuery(query);
+    setAllUsers?.(true); // call if passed
+    debouncedSearch();
+  };
 
   return (
     <SearchInput query={query} onQueryChange={onSearch} inputGroupProps={inputGroupProps} inputProps={inputProps} />
-  )
-})
+  );
+});
