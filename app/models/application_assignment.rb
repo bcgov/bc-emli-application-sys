78ac250faed894054d4ce2eba509f603d10263d0
Jerermy Watts <jeremy.watts@dxcas.com>
2025-07-21 16:18:34 -0700

    feat: fix the quotes on 'permit_application_id'

--- app/models/application_assignment.rb
+++ app/models/application_assignment.rb
@@ -2,7 +2,7 @@ class ApplicationAssignment < ApplicationRecord
   include Auditable
 
   belongs_to :user
-  belongs_to :permit_application, foreign_key: "permit_application_id"
+  belongs_to :permit_application, foreign_key: 'permit_application_id'
 
   validates :user_id, uniqueness: { scope: :permit_application_id }
 end
