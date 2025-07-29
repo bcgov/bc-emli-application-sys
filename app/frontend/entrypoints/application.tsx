ac4c6a62e3cc995ceb0c0570f1b39352ee2dfc5c
Darrel Siegle <darrel.siegle@dxcas.com>
2025-07-29 10:28:23 -0700

    chore: added nonces for styles

--- app/frontend/entrypoints/application.tsx
+++ app/frontend/entrypoints/application.tsx
@@ -1,6 +1,7 @@
 import '@bcgov/bc-sans/css/BC_Sans.css';
 import { ChakraProvider, Flex } from '@chakra-ui/react';
-import { Global } from '@emotion/react';
+import { Global, CacheProvider } from '@emotion/react';
+import createCache from '@emotion/cache';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Helmet } from 'react-helmet';
@@ -15,6 +16,10 @@ import { theme } from '../styles/theme';
 import { useTranslation } from 'react-i18next';
 import '../i18n/i18n';
 
+const nonce = document.querySelector("meta[name='csp-nonce']")?.getAttribute('content') || undefined; //get nonce
+
+const emotionCache = createCache({ key: 'app', nonce }); // set nonce into emotion cache
+
 const renderApp = (rootStore) => {
   const container = document.getElementById('app');
   const root = createRoot(container!);
@@ -24,22 +29,24 @@ const renderApp = (rootStore) => {
     const { t } = useTranslation();
 
     return (
-      <ChakraProvider theme={theme}>
-        <Provider value={rootStore}>
-          <Global styles={GlobalStyles} />
-          <Helmet>
-            <title>{t('site.title')}</title>
-            <meta name="description" content={t('site.metaDescription')} />
-            <meta name="keywords" content={t('site.metaKeywords')} />
-            <meta property="og:title" content={t('site.title')} />
-            <meta property="og:description" content={t('site.metaDescription')} />
-            <meta property="og:image" content="/images/logo.svg" />
-          </Helmet>
-          <Flex direction="column" minH="100vh">
-            <Navigation />
-          </Flex>
-        </Provider>
-      </ChakraProvider>
+      <CacheProvider value={emotionCache}>
+        <ChakraProvider theme={theme}>
+          <Provider value={rootStore}>
+            <Global styles={GlobalStyles} />
+            <Helmet>
+              <title>{t('site.title')}</title>
+              <meta name="description" content={t('site.metaDescription')} />
+              <meta name="keywords" content={t('site.metaKeywords')} />
+              <meta property="og:title" content={t('site.title')} />
+              <meta property="og:description" content={t('site.metaDescription')} />
+              <meta property="og:image" content="/images/logo.svg" />
+            </Helmet>
+            <Flex direction="column" minH="100vh">
+              <Navigation />
+            </Flex>
+          </Provider>
+        </ChakraProvider>
+      </CacheProvider>
     );
   };
   root.render(<RootComponent />);
