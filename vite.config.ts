a7aef0d5de43cf3ae71b70aed38191b14486bcaa
Navdeep Kaur <navdeep.kaur@dxcas.com>
2025-10-20 10:15:48 -0700

    feat: Snowplow analytics integration

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import ReactPlugin from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
-import RubyPlugin from "vite-plugin-ruby"
+import ReactPlugin from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import RubyPlugin from 'vite-plugin-ruby';
 
 export default defineConfig({
   plugins: [RubyPlugin(), ReactPlugin()],
-})
+  optimizeDeps: {
+    include: [
+      '@snowplow/browser-tracker',
+      '@snowplow/browser-plugin-link-click-tracking',
+      '@snowplow/browser-plugin-form-tracking',
+    ],
+  },
+});
