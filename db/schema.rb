a2176b07879c80a73c2096093a8cc360e265588e
Darrel Siegle <darrel.siegle@dxcas.com>
2025-11-25 18:25:43 -0800

    refactor(BCHEP-511): added contractor number field, fixed table missing data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_11_19_235800) do
+ActiveRecord::Schema[7.1].define(version: 2025_11_26_014826) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -178,6 +178,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_11_19_235800) do
     t.boolean "onboarded", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "number"
     t.index ["contact_id"], name: "index_contractors_on_contact_id"
   end
 
