e7d40092ade72099e289b82992053de41dba0f1f
Jeremy Watts <jeremy.watts@dxcas.com>
2026-02-19 15:50:26 -0800

    fix(BCHEP-322): use SiteConfiguration association pattern in migration

--- db/migrate/20260219222612_add_contractor_onboarding_revision_reason.rb
+++ db/migrate/20260219222612_add_contractor_onboarding_revision_reason.rb
@@ -1,14 +1,11 @@
 class AddContractorOnboardingRevisionReason < ActiveRecord::Migration[7.1]
   def up
-    site_config = SiteConfiguration.first
-    return unless site_config
-
-    RevisionReason.find_or_create_by!(
-      reason_code: "contractor_onboarding_form"
-    ) do |r|
-      r.description = "Contractor Onboarding Form"
-      r.site_configuration = site_config
-    end
+    SiteConfiguration
+      .instance
+      .revision_reasons
+      .find_or_create_by(reason_code: "contractor_onboarding_form") do |r|
+        r.description = "Contractor Onboarding Form"
+      end
   end
 
   def down
