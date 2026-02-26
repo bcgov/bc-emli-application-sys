451a24ab0001497426b39804acfdd2b45ee20ec8
Darrel Siegle <darrel.siegle@dxcas.com>
2026-02-26 09:48:50 -0800

    refactor(BCHEP-581): inverted to make intent clearer and less confusing

--- app/frontend/components/domains/energy-savings-application/index.tsx
+++ app/frontend/components/domains/energy-savings-application/index.tsx
@@ -44,7 +44,7 @@ export const EnergySavingsApplicationIndexScreen = observer(
     customButtonLink,
     customEmptyMessage,
   }: IEnergySavingsApplicationIndexScreenProps) => {
-    const SUBMIT_INVOICE_DISABLED = import.meta.env.VITE_SUBMIT_INVOICE_DISABLED === 'true';
+    const SUBMIT_INVOICE_ENABLED = import.meta.env.VITE_SUBMIT_INVOICE_ENABLED === 'true';
     const { t } = useTranslation();
     const { permitApplicationStore, sandboxStore, userStore } = useMst();
     const {
@@ -182,7 +182,7 @@ export const EnergySavingsApplicationIndexScreen = observer(
                       ? `${customButtonText} - ${t('energySavingsApplication.newInvoiceSubmission')}`
                       : t('energySavingsApplication.start')
                   }
-                  isDisabled={SUBMIT_INVOICE_DISABLED}
+                  isDisabled={!SUBMIT_INVOICE_ENABLED}
                 >
                   {customButtonText || t('energySavingsApplication.start')}
                 </RouterLinkButton>
