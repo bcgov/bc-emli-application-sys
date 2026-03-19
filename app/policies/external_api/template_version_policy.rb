4e4f931070714229e5fcbb7df5b87dec6db6b130
Jeremy Watts <jeremy.watts@dxcas.com>
2026-03-19 13:34:50 -0700

    refactor(BCHEP-638): Remove unnecessary subquery from policy scope

--- app/policies/external_api/template_version_policy.rb
+++ app/policies/external_api/template_version_policy.rb
@@ -13,14 +13,7 @@ class ExternalApi::TemplateVersionPolicy < ExternalApi::ApplicationPolicy
         RequirementTemplate.where(
           program_id: external_api_key.program_id
         ).pluck(:id)
-      scope.where(requirement_template_id: rt_ids, status: :published).where(
-        "template_versions.version_date = (
-            SELECT MAX(tv2.version_date) FROM template_versions tv2
-            WHERE tv2.requirement_template_id = template_versions.requirement_template_id
-            AND tv2.status = ?
-          )",
-        TemplateVersion.statuses[:published]
-      )
+      scope.where(requirement_template_id: rt_ids, status: :published)
     end
   end
 end
