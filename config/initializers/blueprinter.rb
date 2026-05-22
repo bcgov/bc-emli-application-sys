2181630803b46fc5b946485c38432d56762ab35f
Jeremy Watts <jeremy.watts@dxcas.com>
2026-05-22 12:14:52 -0700

    fix(BCHEP-674): fix datetime precision and prevent duplicate submission versions
    Fix Blueprinter datetime serialization to use to_f for millisecond precision, preventing frontend sort from treating sub-second submission versions as identical
    
    Wrap submit! in with_lock to prevent duplicate submission versions from concurrent requests

--- config/initializers/blueprinter.rb
+++ config/initializers/blueprinter.rb
@@ -1,6 +1,7 @@
 # epoch in milliseconds needed for js FE
 Blueprinter.configure do |config|
   config.datetime_format = ->(datetime) do
-    (datetime.respond_to?(:to_i) ? datetime.to_i : datetime.to_time.to_i) * 1000
+    dt = datetime.respond_to?(:to_f) ? datetime : datetime.to_time
+    (dt.to_f * 1000).to_i
   end
 end
