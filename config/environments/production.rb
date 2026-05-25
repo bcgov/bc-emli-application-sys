1d9f4ed7a0c65f81a6ada02bcf656201c575f52a
Darrel Siegle <darrel.siegle@dxcas.com>
2026-05-25 15:30:02 -0700

    chore: updated package-lock, reduce heartbeat noise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,6 +60,9 @@ Rails.application.configure do
   # want to log everything, set the level to "debug".
   config.log_level = ENV.fetch("RAILS_LOG_LEVEL", "info")
 
+  # Silence frequent liveness/readiness probes so they do not drown out useful logs.
+  config.silence_healthcheck_path = %r{\A/(health|up)\z}
+
   # Use a different cache store in production.
   if ENV["IS_DOCKER_BUILD"].blank?
     config.cache_store =
