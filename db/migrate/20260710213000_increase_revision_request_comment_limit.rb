d9f7a60dcb1e1f4356d971d690ebd508f4689068
Jeremy Watts <jeremy.watts@dxcas.com>
2026-07-10 17:58:05 -0700

    fix(BCHEP-575): make comment-limit migration reversible

--- db/migrate/20260710213000_increase_revision_request_comment_limit.rb
+++ db/migrate/20260710213000_increase_revision_request_comment_limit.rb
@@ -1,5 +1,9 @@
 class IncreaseRevisionRequestCommentLimit < ActiveRecord::Migration[7.1]
-  def change
+  def up
     change_column :revision_requests, :comment, :string, limit: 1000
+  end
+
+  def down
+    change_column :revision_requests, :comment, :string, limit: 350
   end
 end
