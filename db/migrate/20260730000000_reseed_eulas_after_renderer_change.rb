71bb5dcb6a09efbc4a9957ac8e6e760c06eed47e
Jeremy Watts <jeremy.watts@dxcas.com>
2026-07-31 11:50:07 -0700

    fix(BCHEP-712):  remove raise ActiveRecord::IrreversibleMigration

--- db/migrate/20260730000000_reseed_eulas_after_renderer_change.rb
+++ db/migrate/20260730000000_reseed_eulas_after_renderer_change.rb
@@ -1,11 +1,5 @@
 class ReseedEulasAfterRendererChange < ActiveRecord::Migration[8.1]
-  def up
+  def change
     EulaUpdater.run
-  end
-
-  # Reseeding reads the current eulas/*.html files and overwrites the active
-  # agreement content in place; there is no prior content to restore.
-  def down
-    raise ActiveRecord::IrreversibleMigration
   end
 end
